74b99d38461dc0a38c691ebe7266f4c6841c9d38
unknown <agbrown@METRO-VH224B.ou.ad3.ucdavis.edu>
2015-04-29 10:30:26 -0700

    RStudio project instance commit

--- ct_climate_data_downloader.R
+++ ct_climate_data_downloader.R
@@ -8,11 +8,11 @@ doc.html = htmlTreeParse(base,useInternal = TRUE)
 doc.text = unlist(xpathApply(doc.html, '//a', xmlValue))
 zipfiles=grepl("zip",doc.text)
 
-dir.create("CT_data")
+dir.create("ClimateData")
 
 for(u in doc.text[zipfiles]) {
   url=paste(base,u,sep="")
-  local=paste("CT_Data\\",u,sep="")
+  local=paste("ClimateData\\",u,sep="")
   download.file(url, local)
   unzip(local)
 }
