026a637fbbc2d3606d0a75d0a186864c32f0ca60
Andrew Brown <agbrown@ucdavis.edu>
2015-06-07 18:47:34 -0700

    vGM estimap

--- hydrus_opt.R
+++ hydrus_opt.R
@@ -2,8 +2,8 @@
 #MODELID = 5
 source("ct_methods.R")
 params_MIM=c("modelid","thetar","thetas","alpha","n","ksat","l","thrim","thsim","omega","pulse")
-# params_DUR=c("MODELID","THETAR","THETAS","ALPHA1","ALPHA2","N1","N2","KSAT","L","W2"     ,"PULSE")
-params=params_MIM
+params_DUR=c("MODELID","THETAR","THETAS","ALPHA1","ALPHA2","N1","N2","KSAT","L","W2"     ,"PULSE")
+params=params_DUR
 
 dimension=11
 simdir="C:\\Users\\agbrown\\workspace\\alfalfa_gb\\Simulations_vGM1\\"
