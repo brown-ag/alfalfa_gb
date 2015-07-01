d56ae22b1987bfd32cf1d35294fb983e1f92384f
Andrew Brown <agbrown@ucdavis.edu>
2015-06-30 18:44:08 -0700

    Created Scott Valley alfalfa count R script

---
+++ sv_alfalfa_counts.R
@@ -0,0 +1,16 @@
+foo = read.csv("S:\\Andrew\\CampbellAlfalfa\\27-05-15_SV_bio.csv")
+head(foo)
+
+
+#Alfalfa biomass, using plant count and % cover as covariate
+a=(lm(log10(foo$AlfBag)~foo$Treat+foo$Plant.C+foo$X.Cover))
+summary(a)
+#plot(TukeyHSD(a,conf.level=0.95))
+
+#Weed biomass using alfalfa plant count and % cover  as covariate
+b=(lm(log10(foo$WeedBag)~foo$Treat+foo$Plant.C+foo$X.Cover))
+summary(b)
+plot(foo$WeedBag~foo$Plant.C)
+#plot(TukeyHSD(b,conf.level=0.95))
+
+#MANOVA
