a63de18f593ed682f34c54722b75213bb7feb425
Andrew Brown <agbrown@ucdavis.edu>
2015-07-03 10:51:42 -0700

    Scott Valley alfalfa count script base

--- sv_alfalfa_counts.R
+++ sv_alfalfa_counts.R
@@ -1,10 +1,11 @@
-foo = read.csv("S:\\Andrew\\CampbellAlfalfa\\27-05-15_SV_bio.csv")
+foo = read.csv("27-05-15_SV_bio.csv")
 head(foo)
 
 
 #Alfalfa biomass, using plant count and % cover as covariate
 a=(lm(log10(foo$AlfBag)~foo$Treat+foo$Plant.C+foo$X.Cover))
 summary(a)
+plot(foo$AlfBag~foo$Plant.C)
 #plot(TukeyHSD(a,conf.level=0.95))
 
 #Weed biomass using alfalfa plant count and % cover  as covariate
@@ -14,3 +15,7 @@ plot(foo$WeedBag~foo$Plant.C)
 #plot(TukeyHSD(b,conf.level=0.95))
 
 #MANOVA
+mv1 <- manova(cbind(foo$AlfBag,foo$WeedBag) ~ foo$Treat+foo$Plant.C+foo$X.Cover, data=foo)
+summary(mv1,test="W")
+summary(mv1,test="H")
+summary(mv1,test="P")
